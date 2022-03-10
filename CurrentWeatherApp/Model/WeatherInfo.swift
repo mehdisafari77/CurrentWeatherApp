f26d4779b8e957262b85fb461c633483b4ac0e41
Mehdi Safari <75599021+mehdisafari77@users.noreply.github.com>
2022-03-10 08:29:37 -0800

    create CodingKey to extract weather data

--- CurrentWeatherApp/Model/WeatherInfo.swift
+++ CurrentWeatherApp/Model/WeatherInfo.swift
@@ -9,6 +9,26 @@ import Foundation
 
 struct WeatherInfo: Decodable {
 
-   
+    var coord: Coord
+    var name: String
+    var main: Main
 }
+
+struct Coord: Decodable {
+    var lon: Double
+    var lat: Double
+}
+
+struct Main: Decodable {
+    var temp: Double
+    var tempMin: Double
+    var tempMax: Double
+    
+    enum CodingKeys: String, CodingKey {
+        case tempMin = "temp_min"
+        case tempMax = "temp_max"
+        case temp = "temp"
+    }
+}
+
 
